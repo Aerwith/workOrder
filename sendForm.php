17e7fa3a068240aa74e9fb786f25d9aa9e10853d
Aerwith <alexmajeska@gmail.com>
2015-06-09 17:09:13 -0400

    Update sendForm.php

--- sendForm.php
+++ sendForm.php
@@ -1,7 +1,19 @@
 <?php
 //Connecting to sql db.
-$connect = mysqli_connect(   );
+date_default_timezone_set('America/New_York');
+$connect = mysqli_connect("localhost",$_POST['dbuser'],$_POST['dbpass'],"maint");
 //Sending form data to sql db.
-mysqli_query($connect,"INSERT INTO pending (DateField, Issue, Time, Equipment, ProductLine) 
-	VALUES ('$_POST[DateField]', '$_POST[Issue]', '$_POST[Time]', '$_POST[Equipment]', '$_POST[ProductLine]')");
+$date = strtotime($_POST['DateField']);
+echo $date;
+$query = sprintf("INSERT INTO pending (DateField, Issue, Time, CN, ProductLine, Status) 
+	VALUES ('%s', '%s', '%s', '%s', '%s','%s')",
+		$date,
+    		$_POST['Issue'],
+		$_POST['Time'],
+		$_POST['Equipment'],
+		$_POST['ProductLine'],
+		'Pending');
+echo $query;
+mysqli_query($connect, $query);
+
 ?>
